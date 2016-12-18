c35fdbb6caf4e6ac7d0726baac12a586f5e39463
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-12-18 23:24:31 +0900

    commit

---
+++ Assets/Plugins/WebGL/MyPlugin.jslib
@@ -0,0 +1,85 @@
+//var cbobj, HEAP8, HEAPU8, HEAP16, HEAPU16, HEAP32, HEAPU32, HEAPF32, HEAPF64, Runtime, _malloc, _free, autoAddDeps, mergeInto, LibraryManager;
+
+var MyPlugin = {
+    $cbobj: {},
+
+    $receiveDataFromUnity: function(type, heap, ptr, size) {
+        var strVals = [];
+        debugger;
+        heap.subarray(ptr / heap.BYTES_PER_ELEMENT, (ptr / heap.BYTES_PER_ELEMENT) + size).forEach(function(val) {
+            strVals.push('' + val);
+        });
+        console.log(type + 'ArrayTest: [' + strVals.join(',') + ']');
+    },
+
+    $sendDataToUnity: function(data, heap, callback) {
+        var writeData = typeof data === 'string' ? (new TextEncoder).encode(data + String.fromCharCode(0)) : data;
+        var buffer = _malloc(writeData.length * (data.BYTES_PER_ELEMENT || 1));
+        debugger;
+        heap.set(writeData, buffer / (data.BYTES_PER_ELEMENT || 1));
+        Runtime.dynCall('vii', callback, [buffer, writeData.length]);
+        _free(buffer);
+    },
+
+    initCallback: function(onText, onByteArray, onSByteArray, onShortArray, onUShortArray, onIntArray, onUIntArray, onFloatArray, onDoubleArray) {
+        cbobj.onText = onText;
+        cbobj.onByteArray = onByteArray;
+        cbobj.onSByteArray = onSByteArray;
+        cbobj.onShortArray = onShortArray;
+        cbobj.onUShortArray = onUShortArray;
+        cbobj.onIntArray = onIntArray;
+        cbobj.onUIntArray = onUIntArray;
+        cbobj.onFloatArray = onFloatArray;
+        cbobj.onDoubleArray = onDoubleArray;
+    },
+
+    sbyteArrayTest: function(ptr, size) {
+        receiveDataFromUnity('sbyte', HEAP8, ptr, size);
+    },
+
+    byteArrayTest: function(ptr, size) {
+        receiveDataFromUnity('byte', HEAPU8, ptr, size);
+    },
+
+    shortArrayTest: function(ptr, size) {
+        receiveDataFromUnity('short', HEAP16, ptr, size);
+    },
+
+    ushortArrayTest: function(ptr, size) {
+        receiveDataFromUnity('ushort', HEAPU16, ptr, size);
+    },
+
+    intArrayTest: function(ptr, size) {
+        receiveDataFromUnity('int', HEAP32, ptr, size);
+    },
+
+    uintArrayTest: function(ptr, size) {
+        receiveDataFromUnity('uint', HEAPU32, ptr, size);
+    },
+
+    floatArrayTest: function(ptr, size) {
+        receiveDataFromUnity('float', HEAPF32, ptr, size);
+    },
+
+    doubleArrayTest: function(ptr, size) {
+        receiveDataFromUnity('double', HEAPF64, ptr, size);
+    },
+
+    testSendDataToUnity: function() {
+        sendDataToUnity('hoge_fuga_piyo', HEAPU8, cbobj.onText);
+        sendDataToUnity('abc𩸽def', HEAPU8, cbobj.onText);
+        sendDataToUnity(new Int8Array([9, 1, 2, 3]), HEAP8, cbobj.onSByteArray);
+        sendDataToUnity(new Uint8Array([9, 1, 2, 3]), HEAPU8, cbobj.onByteArray);
+        sendDataToUnity(new Int16Array([9, 1, 2, 3]), HEAP16, cbobj.onShortArray);
+        sendDataToUnity(new Uint16Array([9, 1, 2, 3]), HEAPU16, cbobj.onUShortArray);
+        sendDataToUnity(new Int32Array([9, 1, 2, 3]), HEAP32, cbobj.onIntArray);
+        sendDataToUnity(new Uint32Array([9, 1, 2, 3]), HEAPU32, cbobj.onUIntArray);
+        sendDataToUnity(new Float32Array([9, 1, 2, 3]), HEAPF32, cbobj.onFloatArray);
+        sendDataToUnity(new Float64Array([9, 1, 2, 3]), HEAPF64, cbobj.onDoubleArray);
+    }
+};
+
+autoAddDeps(MyPlugin, '$cbobj');
+autoAddDeps(MyPlugin, '$receiveDataFromUnity');
+autoAddDeps(MyPlugin, '$sendDataToUnity');
+mergeInto(LibraryManager.library, MyPlugin);
---
+++ Assets/jslibbridge.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+using AOT;
+using System.Collections.Generic;
+using System.Text;
+
+public class jslibbridge : MonoBehaviour {
+
+    [DllImport("__Internal")]
+    private static extern void byteArrayTest(byte[] data, int size);
+
+    [DllImport("__Internal")]
+    private static extern void sbyteArrayTest(sbyte[] data, int size);
+
+    [DllImport("__Internal")]
+    private static extern void shortArrayTest(short[] data, int size);
+
+    [DllImport("__Internal")]
+    private static extern void ushortArrayTest(ushort[] data, int size);
+
+    [DllImport("__Internal")]
+    private static extern void intArrayTest(int[] data, int size);
+
+    [DllImport("__Internal")]
+    private static extern void uintArrayTest(uint[] data, int size);
+
+    [DllImport("__Internal")]
+    private static extern void floatArrayTest(float[] data, int size);
+
+    [DllImport("__Internal")]
+    private static extern void doubleArrayTest(double[] data, int size);
+
+    delegate void dlgReceiveTextData(string txt, int len);
+    delegate void dlgReceiveByteArrayData(byte[] data, int length);
+    delegate void dlgReceiveSByteArrayData(sbyte[] data, int length);
+    delegate void dlgReceiveShortArrayData(short[] data, int length);
+    delegate void dlgReceiveUShortArrayData(ushort[] data, int length);
+    delegate void dlgReceiveIntArrayData(int[] data, int length);
+    delegate void dlgReceiveUIntArrayData(uint[] data, int length);
+    delegate void dlgReceiveFloatArrayData(float[] data, int length);
+    delegate void dlgReceiveDoubleArrayData(double[] data, int length);
+
+    [DllImport("__Internal")]
+    static extern uint initCallback(
+        dlgReceiveTextData onText,
+        dlgReceiveSByteArrayData onSByteArray,
+        dlgReceiveByteArrayData onByteArray,
+        dlgReceiveShortArrayData onShortArray,
+        dlgReceiveUShortArrayData onUShortArray,
+        dlgReceiveIntArrayData onIntArray,
+        dlgReceiveUIntArrayData onUIntArray,
+        dlgReceiveFloatArrayData onFloatArray,
+        dlgReceiveDoubleArrayData onDoubleArray
+    );
+
+    [MonoPInvokeCallback(typeof(dlgReceiveTextData))]
+    static void OnText(string txt, int len)
+    {
+        Debug.Log("recieve text: " + txt);
+    }
+
+    static void debuglogReceiveData<T>(string type, T[] data, int length)
+    {
+        var sb = new StringBuilder();
+        sb.Append("Receive " + type + " array: (length: " + length + ") [");
+        var spl = "";
+        for (var i = 0; i < length; i++)
+        {
+            sb.Append(spl + data[i]);
+            spl = ", ";
+        }
+        sb.Append("]");
+        Debug.Log(sb.ToString());
+    }
+
+    [MonoPInvokeCallback(typeof(dlgReceiveSByteArrayData))]
+    static void OnSByteArray([MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.I1, SizeParamIndex = 1)] sbyte[] data, int length)
+    {
+        debuglogReceiveData<sbyte>("sbyte", data, length);
+    }
+
+    [MonoPInvokeCallback(typeof(dlgReceiveByteArrayData))]
+    static void OnByteArray([MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U1, SizeParamIndex = 1)] byte[] data, int length)
+    {
+        debuglogReceiveData<byte>("byte", data, length);
+    }
+
+    [MonoPInvokeCallback(typeof(dlgReceiveShortArrayData))]
+    static void OnShortArray([MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.I2, SizeParamIndex = 1)] short[] data, int length)
+    {
+        debuglogReceiveData<short>("short", data, length);
+    }
+
+    [MonoPInvokeCallback(typeof(dlgReceiveUShortArrayData))]
+    static void OnUShortArray([MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U2, SizeParamIndex = 1)] ushort[] data, int length)
+    {
+        debuglogReceiveData<ushort>("ushort", data, length);
+    }
+
+    [MonoPInvokeCallback(typeof(dlgReceiveIntArrayData))]
+    static void OnIntArray([MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.I4, SizeParamIndex = 1)] int[] data, int length)
+    {
+        debuglogReceiveData<int>("int", data, length);
+    }
+
+    [MonoPInvokeCallback(typeof(dlgReceiveUIntArrayData))]
+    static void OnUIntArray([MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U4, SizeParamIndex = 1)] uint[] data, int length)
+    {
+        debuglogReceiveData<uint>("uint", data, length);
+    }
+
+    [MonoPInvokeCallback(typeof(dlgReceiveFloatArrayData))]
+    static void OnFloatArray([MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.R4, SizeParamIndex = 1)] float[] data, int length)
+    {
+        debuglogReceiveData<float>("float", data, length);
+    }
+
+    [MonoPInvokeCallback(typeof(dlgReceiveDoubleArrayData))]
+    static void OnDoubleArray([MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.R8, SizeParamIndex = 1)] double[] data, int length)
+    {
+        debuglogReceiveData<double>("double", data, length);
+    }
+
+    [DllImport("__Internal")]
+    private static extern void testSendDataToUnity();
+
+    void Start()
+    {
+        initCallback(OnText, OnSByteArray, OnByteArray, OnShortArray, OnUShortArray, OnIntArray, OnUIntArray, OnFloatArray, OnDoubleArray);
+
+        sbyteArrayTest(new sbyte[] { 1, 2, 3 }, 3);
+        byteArrayTest(new byte[] { 1, 2, 3 }, 3);
+        shortArrayTest(new short[] { 1, 2, 3 }, 3);
+        ushortArrayTest(new ushort[] { 1, 2, 3 }, 3);
+        intArrayTest(new int[] { 1, 2, 3 }, 3);
+        uintArrayTest(new uint[] { 1, 2, 3 }, 3);
+        floatArrayTest(new float[] { 1, 2, 3 }, 3);
+        doubleArrayTest(new double[] { 1, 2, 3 }, 3);
+
+        testSendDataToUnity();
+    }
+
+}
